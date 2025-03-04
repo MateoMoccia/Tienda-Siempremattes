87c86f287f400b1e34256f08c95c09c7caaf0d9a
Mateo Moccia <mateomoccia@gmail.com>
2025-03-04 12:16:56 -0300

    Actualizacion descipciones

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Productos } from '../ProductosRndLista/ProductosLista';  // Asegúrate de que esta importación es correcta
+import { Productos } from '../ProductosRndLista/ProductosLista';
 import ItemDetail from '../ItemDetail/ItemDetail';
 
 const ItemDetailContainer = () => {
   const { id } = useParams();
-  console.log("ID recibido de useParams:", id);  // Verifica si el id es correcto
   const [producto, setProducto] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -15,26 +14,15 @@ const ItemDetailContainer = () => {
       const productoEncontrado = Productos.find((p) => p.id === Number(id));
       if (productoEncontrado) {
         setProducto(productoEncontrado);
-      } else {
-        setProducto(null);
       }
     }
     setLoading(false);
   }, [id]);
 
-  if (loading) {
-    return <p>Cargando...</p>;
-  }
+  if (loading) return <p>Cargando...</p>;
+  if (!producto) return <p>Producto no encontrado.</p>;
 
-  if (!producto) {
-    return <p>Producto no encontrado.</p>;
-  }
-
-  return (
-    <div>
-      <ItemDetail producto={producto} />
-    </div>
-  );
+  return <ItemDetail producto={producto} />;
 };
 
 export default ItemDetailContainer;
--- src/components/ProductoRnd/ProductoRnd.jsx
+++ src/components/ProductoRnd/ProductoRnd.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './ProductoRnd.css';
 import { Link } from 'react-router-dom';
 
-const ProductoRnd = ({ id, imagen, nombre, precio, sinExtras }) => {
+const ProductoRnd = ({ id, imagen, nombre, precio, descripcion, sinExtras }) => {
   return (
     <div className='ProductosContainer'>
       <div className='ProductoRnd'>
@@ -10,14 +10,11 @@ const ProductoRnd = ({ id, imagen, nombre, precio, sinExtras }) => {
           <img src={imagen} alt={nombre} className='ImagenProductos' />
         </div>
         <h3 className='NombreProductos'>{nombre}</h3>
-        {/* Mostrar el precio solo si sinExtras es falso */}
         {!sinExtras && <p className='PrecioProductos'>{precio}$</p>}
-        
-        {/* Mostrar el botón solo si sinExtras es falso */}
         {!sinExtras && (
-        <Link to={`/detalle/${id}`} className='btn-detalles'>
-        <p>Ver detalles</p>
-      </Link>      
+          <Link to={`/detalle/${id}`} className='btn-detalles'>
+            <p>Ver detalles</p>
+          </Link>
         )}
       </div>
     </div>
