7d9d5906541a55dac59f32d1ab1ac2968ab76b7f
Mateo Moccia <mateomoccia@gmail.com>
2025-06-16 02:24:33 -0300

    actualizacion de grabado de mates

--- src/pages/Mates.jsx
+++ src/pages/Mates.jsx
@@ -1,11 +1,16 @@
 import React, { useContext } from 'react';
 import ProductosLista from '../components/ProductosRndLista/ProductosLista';
+import '../Styles/Mates.css';
 
 const Mates = () => {
 
   return (
     <div>
       <h2>Mates </h2>
+      <div className='container-grabado-mates'>
+        <p className='GrabadoDeMates'>Presentamos nuestra opcion de grabado de mates totalmente personalizado</p>
+                  <img src="/img/Grabado-de-mates.png" alt="ImagenGrabados" />
+      </div>
       <ProductosLista categoria="Mates"/>
     </div>
   );
