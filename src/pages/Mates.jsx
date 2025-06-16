66c235a544d8a17bf2923d7da7f54e262b6d9fad
Mateo Moccia <mateomoccia@gmail.com>
2025-06-16 12:51:04 -0300

    actualizacion mensaje de grabado3

--- src/pages/Mates.jsx
+++ src/pages/Mates.jsx
@@ -8,8 +8,9 @@ const Mates = () => {
     <div>
       <h2>Mates </h2>
       <div className='container-grabado-mates'>
-        <p className='GrabadoDeMates'>Presentamos nuestra opcion de grabado de mates totalmente personalizado</p>
-                <p className='GrabadoDeMates2'>Contactanos a nuestras redes,mail o numero para tener el tuyo totalmente a tu gusto</p>
+        <p className='GrabadoDeMates'>Presentamos nuestra opcion de grabado de mates totalmente personalizado.
+          Que esperas para contactarnos y obtener el tuyo!!
+        </p>
                   <img className='ImagenGrabados' src="/img/Grabado-de-mates.png" alt="ImagenGrabados" />
       </div>
       <ProductosLista categoria="Mates"/>
